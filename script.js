5b7f45156968b7821715628e4572b03d6c9b62d2
Alex Grafen <alex.grafen@gmail.com>
2021-11-23 18:28:56 +0000

    some initial js and testing

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let languageButton = document.getElementById("language-button")
+
+function changeLanguage() {
+    if (lang="en") {
+        Element.hidden = true;
+    }
+    Element.hidden = false;
+}
+
+languageButton.addEventListener('click', changeLanguage)
+
+/*a function that changes elements with 
+lang="en" to .hidden = true and lang="yi" 
+to .hidden = false and vice versa when 
+clicked again
+*/
